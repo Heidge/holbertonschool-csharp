06388c9909dced1dec6d8d8f5c88ecc961429bfd
Axel <axel.gore35@gmail.com>
2023-07-24 13:37:50 +0200

    fix run of main file

--- csharp-hello_world/3-writeline/3-writeline.cs
+++ csharp-hello_world/3-writeline/3-writeline.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("\"Programming is like building a multilingual puzzle");
+﻿using System;
+
+namespace Writeline
+{
+    class 3_writeline 
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("\"Programming is like building a multilingual puzzle");
+        }  
+    }
+}
