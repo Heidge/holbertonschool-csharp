c0d506b962b00e4cd302733662ace5130c7f4418
Heidge <axel.gore@outlook.fr>
2023-07-24 15:48:35 +0200

    push task 9 files

--- csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -7,6 +7,6 @@ class Program
         double percent = .7553;
         double currency = 98765.4321;
         Console.WriteLine("Percent: {0:P}", percent);
-        Console.WriteLine(currency.ToString("C", CultureInfo.CreateSpecificCulture("en-US")));
+        Console.WriteLine("Currency: " + currency.ToString("C", CultureInfo.CreateSpecificCulture("en-US")));
     }
 }
