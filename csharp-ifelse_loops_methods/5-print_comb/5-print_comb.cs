564dd1c0a1a7c7482c073e20ebaa49eb9c1869f4
Heidge <axel.gore@outlook.fr>
2023-07-25 12:53:50 +0200

    fix with new format index

--- csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -7,22 +7,11 @@ namespace _2_print_alphabet
         static void Main(string[] args)
         {
             int i = 0;
-            int j = 0;
-            int k = 0;
             for (i = 0; i <= 99; i++)
             {
                 if (i < 99)
                 {
-                    if (k < 10 && j < 10)
-                    {
-                        Console.Write($"{k}{j}, ");
-            
-                    }
-                    else
-                    {
-                        Console.Write($"{j}, ");
-                    }
-                    j++;
+                   Console.Write($"{i:D2}, ");
                 }
                 else
                 {
