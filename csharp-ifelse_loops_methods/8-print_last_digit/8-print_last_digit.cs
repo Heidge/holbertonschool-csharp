92ef7f6e1ea4ccec011da0218c08196d102221b3
Heidge <axel.gore@outlook.fr>
2023-07-25 14:30:19 +0200

    push task 8 files

--- csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
+++ csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -6,7 +6,24 @@ namespace _8_print_last_digit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int r;
+
+            Number.PrintLastDigit(98);
+            Number.PrintLastDigit(0);
+            r = Number.PrintLastDigit(-1024);
+            Console.WriteLine(r);
+        }
+    }
+
+    class Number
+    {
+        public static int PrintLastDigit(int number)
+        {
+            string numberString = number.ToString();
+            int numberLength = numberString.Length;
+            string lastDigit = numberString.Substring(numberLength - 1, 1);
+            int tointLastDigit = Convert.ToInt32(lastDigit);
+            return (tointLastDigit);
         }
     }
 }
