630ccdd32e60e9c41806af2f32bd50f15eaf294a
Heidge <axel.gore@outlook.fr>
2023-07-25 13:10:05 +0200

    push task 6 files

--- csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
+++ csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 
-namespace _6_print_comb2
+namespace _2_print_alphabet
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int i = 0;
+            int k = 1;
+
+            for (i = 0; i <= 9; i++)
+            {
+                for (int j = k; j <= 9; j++)
+                {
+                    if (i < 8)
+                    {
+                        Console.Write(i);
+                        Console.Write($"{j}, ");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{i}{j}");
+                    }
+                }
+                k++;
+            }
         }
     }
 }
