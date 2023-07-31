946425d798abf0846991df5e6d3e434a9fec914b
Heidge <axel.gore@outlook.fr>
2023-07-31 13:18:26 +0200

    push task 0 files

---
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _0_print_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] newArray;
+
+            newArray = Array.CreatePrint(10);
+            Console.WriteLine("Array Length: " + newArray.Length);
+            Console.WriteLine("----------------");
+            newArray = Array.CreatePrint(16);
+            Console.WriteLine("Array Length: " + newArray.Length);
+            Console.WriteLine("----------------");
+            newArray = Array.CreatePrint(0);
+            Console.WriteLine("Array Length: " + newArray.Length);
+            Console.WriteLine("----------------");
+            newArray = Array.CreatePrint(-10);
+        }
+    }
+    class Array
+    {
+        public static int[] CreatePrint(int size)
+        {
+            if (size > 0)
+            {
+                int[] Array = new int[size];
+                return Array;
+            }
+            else if (size == 0)
+            {
+                Console.WriteLine("");
+                int[] Array = new int[size];
+                return Array;
+            }
+            else
+            {
+                Console.WriteLine("Size cannot be negative");
+                return null;
+            }
+
+        }
+    }
+}
+
+
