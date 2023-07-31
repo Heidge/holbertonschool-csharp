4b3986885b2450d5b6550c91edd1d994c1d28306
Heidge <axel.gore@outlook.fr>
2023-07-31 14:29:44 +0200

    push task 1 files

---
+++ csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public static class Array
+{
+    public static int elementAt(int[] array, int index)
+    {
+        int arrayLength = array.Length;
+        if (index >= 0 && index < arrayLength)
+        {
+            return (array[index]);
+            
+        }
+        else
+        {
+           Console.WriteLine("Index out of range");
+            return (-1);
+        }
+    }
+}
+
+
+
