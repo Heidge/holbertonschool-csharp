2265b73ea6d8289c44e838022d7b3112efc25d88
Heidge <axel.gore@outlook.fr>
2023-07-31 17:16:34 +0200

    push task 6 files

---
+++ csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        int lengthMyList = myList.Count;
+        List<bool> retVal = new List<bool>(new bool[lengthMyList]);
+
+        for (int i = 0; i < myList.Count; i++)
+        {
+            if (myList[i] % 2 == 0)
+            {
+                retVal[i] = true;
+            }
+            else
+            {
+                retVal[i] = false;
+            }
+        }
+        return retVal;
+    }
+}
+
