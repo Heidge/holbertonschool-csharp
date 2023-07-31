36ff5c23fa0dfcbfbf3655f5eacf2dd06f073be5
Heidge <axel.gore@outlook.fr>
2023-07-31 16:26:39 +0200

    push task 5 files

---
+++ csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int MaxInteger(List<int> myList)
+    {
+        if (myList == null)
+        {
+            Console.WriteLine("List is empty");
+            return -1;
+        }
+        else
+        {
+            int maxNumber = 0;
+            for (int i = 0; i < myList.Count; i++)
+            {
+                if (myList[i] > maxNumber)
+                {
+                    maxNumber = myList[i];
+                }
+            }
+            return maxNumber;
+        }
+    }
+}
+
