eddb9e9d23ddf308f7fdd998419690de6caeb030
Heidge <axel.gore35@gmail.com>
2023-08-01 11:56:11 +0200

    push task 12 files

---
+++ csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Dictionary
+{
+    public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+        var dictSorted = myDict.OrderBy(x => x.Key).ToDictionary(x => x.Key, x => x.Value);
+
+        foreach (var kvp in dictSorted)
+        {
+            Console.WriteLine($"{kvp.Key}: {kvp.Value}");
+        }
+    }
+}
+
