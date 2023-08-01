b2a1240af5d31766d1e2f30be483e15f89ee1799
Heidge <axel.gore35@gmail.com>
2023-08-01 12:22:11 +0200

    push task 13 files

---
+++ csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        int bestScore = 0;
+        string bestPeople = "";
+
+        foreach (var kvp in myList)
+        {
+            if (kvp.Value > bestScore)
+            {
+                bestScore = kvp.Value;
+            }
+        }
+        
+        foreach (KeyValuePair<string, int> kvp in myList)
+        {
+            if (bestScore == kvp.Value)
+            {
+                bestPeople = kvp.Key;
+                break;
+            }
+            else
+            {
+                bestPeople = "None";
+            }
+        }
+        return bestPeople;
+    }
+}
+
