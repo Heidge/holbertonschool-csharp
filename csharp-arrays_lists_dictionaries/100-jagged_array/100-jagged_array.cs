97fb2fd503882d2bcdba03bc23aa6dffb99b5403
Heidge <axel.gore35@gmail.com>
2023-08-01 14:11:26 +0200

    push task 100 files

--- csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
+++ csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
@@ -11,14 +11,19 @@ public static class Array
         jaggedArray[1] = new int[] { 0, 1, 2, 3, 4, 5, 6 };
         jaggedArray[2] = new int[] { 0, 1 };
 
-
         for (int i = 0; i < jaggedArray.Length; i++)
         {
             for (int j = 0; j < jaggedArray[i].Length; j++)
             {
-                Console.Write("{0} ", jaggedArray[i][j]);
+                if (j == jaggedArray[i].Length - 1)
+                {
+                    Console.WriteLine("{0}", jaggedArray[i][j]);
+                }
+                else
+                {
+                    Console.Write("{0} ", jaggedArray[i][j]);
+                }
             }
-            Console.WriteLine();
-        } 
+        }
     }
 }
