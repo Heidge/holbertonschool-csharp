448e9e9c58f37a7dbc3b9f115549facf2dcf3dcb
Heidge <axel.gore35@gmail.com>
2023-08-01 12:45:36 +0200

    push task 14 files

---
+++ csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class Array
+{
+    static void Main(string[] args)
+    {
+        int[] array = new int[5];
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            for (int j = 0; j < array.Length; j++)
+            {
+                if (i == 2 && j == 2)
+                {
+                    Console.Write("1 ");
+                }
+                else
+                {
+                    Console.Write($"{array[i]} ");
+                }
+
+            }
+            Console.WriteLine();
+        }
+    }
+}
