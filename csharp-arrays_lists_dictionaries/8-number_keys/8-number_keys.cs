2662e15c5994e17dce03a141e91474ba0c448946
Heidge <axel.gore35@gmail.com>
2023-08-01 10:53:07 +0200

    push task 8 files

---
+++ csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class Dictionary
+    {
+        public static int NumberOfKeys(Dictionary<string, string> myDict)
+        {
+            int nbKeys = 0;
+            foreach (var key in myDict)
+            {
+                nbKeys++;
+            }
+            return nbKeys;
+        }
+    }
+
