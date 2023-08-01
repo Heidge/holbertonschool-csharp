033f78e6be2f5613aeb599c754ebf2dae6b26c9f
Heidge <axel.gore35@gmail.com>
2023-08-01 13:03:49 +0200

    push task 15 files

---
+++ csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+    class Matrix
+    {
+        public static int[,] Square(int[,] myMatrix)
+        {
+            int[,] array2D = new int[3, 3];
+            
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    array2D[i, j] = myMatrix[i, j] * myMatrix[i, j];
+                }
+            }
+            return array2D;
+        }
+    }
+
