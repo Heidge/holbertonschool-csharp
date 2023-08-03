fe3cada4797d2651de435c10c38518b11c148871
Heidge <axel.gore35@gmail.com>
2023-08-03 10:28:50 +0200

    push task 1 files

--- csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
+++ csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -5,6 +5,17 @@ using System.Collections.Generic;
     {
         public static List<int> CommonElements(List<int> list1, List<int> list2)
         {
-            Console.WriteLine("Hello World!");
+            List<int> newList = new List<int>();
+            foreach (var item1 in list1)
+            {
+                foreach (var item2 in list2)
+                {
+                    if (item1 == item2)
+                    {
+                        newList.Add(item1);
+                    }
+                }
+            }
+            return newList;
         }
     }
