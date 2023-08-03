17ae21d321f24bf35650091cc7dc6c499d2a017e
Heidge <axel.gore35@gmail.com>
2023-08-03 13:58:40 +0200

    push task 5 files

--- csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
+++ csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -1,10 +1,24 @@
 ﻿using System;
+using System.Text;
 using System.Collections.Generic;
 
-    class LList
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
     {
-        public static LinkedList<int> CreatePrint(int size)
+        LinkedList<int> newLlist = new LinkedList<int>();
+        if (size < 0)
         {
-            Console.WriteLine("Hello World!");
+            return newLlist;
+        }
+        else
+        {
+            for (int i = 0; i < size; i++)
+            {
+                newLlist.AddLast(i);
+                Console.WriteLine($"{i}");
+            }
+            return newLlist;
         }
     }
+}
