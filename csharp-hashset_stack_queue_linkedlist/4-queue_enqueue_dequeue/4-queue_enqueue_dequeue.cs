199dd665bb0e294ca83a2f89dfab1898361199e1
Heidge <axel.gore35@gmail.com>
2023-08-03 13:40:47 +0200

    push task 5 files

--- csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
+++ csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -6,6 +6,39 @@ using System.Collections.Generic;
     {
         public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
         {
-            Console.WriteLine("Hello World!");
+            int CountAQueue = aQueue.Count;
+
+        Console.WriteLine($"Number of items: {CountAQueue}");
+
+        if (aQueue.Count == 0)
+        {
+            Console.WriteLine("Queue is empty");
+        }
+        else
+        {
+            Console.WriteLine("Top item: {0}", aQueue.Peek());
+        }
+
+        bool match = false;
+        foreach (string index in aQueue)
+        {
+            if (index == search)
+            {
+                match = true;
+            }
+        }
+        Console.WriteLine($"Queue contains \"{search}\": {match}");
+
+        if (aQueue.Contains(search))
+        {
+            while (aQueue.Dequeue() != search)
+            {
+                continue;
+            }
+        }
+
+        aQueue.Enqueue(newItem);
+
+        return aQueue;
         }
     }
