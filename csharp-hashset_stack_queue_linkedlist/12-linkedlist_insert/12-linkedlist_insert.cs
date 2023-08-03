f2caf06ece39229d60ac23101be3e46c085a9948
Heidge <axel.gore35@gmail.com>
2023-08-03 16:03:41 +0200

    push task 10 files

--- csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
+++ csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -1,11 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 
-    class LList
+class LList
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
     {
-        public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+        LinkedListNode<int> currentNode = myLList.First;
+
+        while (currentNode != null && currentNode.Value < n)
         {
-            Console.WriteLine("Hello World!");
+            currentNode = currentNode.Next;
         }
+
+        // Insérer le nouveau nœud avant le currentNode (si currentNode est null, cela signifie que le nombre doit être inséré à la fin)
+        if (currentNode != null)
+        {
+            myLList.AddBefore(currentNode, n);
+        }
+        else
+        {
+            myLList.AddLast(n);
+        }
+
+        return currentNode;
+
     }
+}
 
