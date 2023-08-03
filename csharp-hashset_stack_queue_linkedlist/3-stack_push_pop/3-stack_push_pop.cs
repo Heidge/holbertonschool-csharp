fc55ccd27c6cb100b6c9d1f58d3f8160ca0a0cb6
Heidge <axel.gore35@gmail.com>
2023-08-03 12:44:53 +0200

    push task 3 files

--- csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -1,10 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 
-    class MyStack
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
     {
-        public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+        int CountAStack = aStack.Count;
+
+        Console.WriteLine($"Number of items: {CountAStack}");
+
+        if (aStack.Count == 0)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Stack is empty");
         }
+        else
+        {
+            Console.WriteLine("Top item: {0}", aStack.Peek());
+        }
+
+        bool match = false;
+        foreach (string index in aStack)
+        {
+            if (index == search)
+            {
+                match = true;
+            }
+        }
+        Console.WriteLine($"Stack contains \"{search}\": {match}");
+
+        if (aStack.Contains(search))
+        {
+            while (aStack.Pop() != search)
+            {
+                continue;
+            }
+        }
+
+        aStack.Push(newItem);
+
+        return aStack;
     }
+}
