b1166b3ee2259585e1b72095e66f3a21d487d21b
Heidge <axel.gore35@gmail.com>
2023-08-03 16:29:32 +0200

    push task 13 files

--- csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
+++ csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -6,24 +6,14 @@ class LList
     public static void Delete(LinkedList<int> myLList, int index)
     {
 
-        if (index <= 0)
+
+        LinkedListNode<int> nodeToRemove = myLList.First;
+        for (int i = 0; i < index; i++)
         {
-            LinkedListNode<int> nodeToRemove = myLList.First;
-            myLList.Remove(nodeToRemove);
+            nodeToRemove = nodeToRemove.Next;
         }
-        else if (index > myLList.Count)
-        {
-            myLList.Clear();
-        }
-        else
-        {
-            LinkedListNode<int> nodeToRemove = myLList.First;
-            for (int i = 0; i < index; i++)
-            {
-                nodeToRemove = nodeToRemove.Next;
-            }
-            myLList.Remove(nodeToRemove);
-        }
+        myLList.Remove(nodeToRemove);
+
     }
 }
 
