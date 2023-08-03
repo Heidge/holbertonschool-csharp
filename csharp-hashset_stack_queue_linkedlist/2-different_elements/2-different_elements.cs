1f9b8775f734b05be3dba606b12579f1014c6ccf
Heidge <axel.gore35@gmail.com>
2023-08-03 11:18:25 +0200

    push task 2 files

--- csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
+++ csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -1,10 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 
-    class List
+class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
     {
-        public static List<int> DifferentElements(List<int> list1, List<int> list2)
+        List<int> newList = GetDifference(list1, list2);
+        List<int> newList2 = GetDifference(list2, list1);
+
+        foreach (var item in newList2)
         {
-            Console.WriteLine("Hello World!");
+            newList.Add(item);
         }
+
+        List<int> GetDifference(List<int> list3, List<int> list4)
+        {
+            List<int> difference = new List<int>();
+
+            foreach (int element in list3)
+            {
+                if (!list4.Contains(element))
+                {
+                    difference.Add(element);
+                }
+            }
+
+            return difference;
+        }
+        newList.Sort();
+        return newList;
     }
+}
