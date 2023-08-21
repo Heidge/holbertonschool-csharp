8fe3c9bee46b278da611734d88b42fa124bd2cd3
Heidge <axel.gore@outlook.fr>
2023-08-21 16:03:48 +0200

    push task 3 files

--- csharp-classes/2-enemy/2-enemy.cs
+++ csharp-classes/2-enemy/2-enemy.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 
-namespace _2_enemy
+namespace Enemies
 {
-    class Program
+/// <summary>
+/// New class zombie
+/// </summary>
+    public class Zombie
+
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// health field
+        /// </summary>
+        public int health;
+        /// <summary>
+        /// new constructor for zombie
+        /// </summary>
+        public Zombie()
         {
-            Console.WriteLine("Hello World!");
+            health = 0;
+        }
+
+        public Zombie(int value)
+        {
+            if (value < 0)
+                throw new ArgumentException("Health must be greater than or equal to 0");
         }
     }
 }
