62d5daeb759a13b1da5378079d7a3b2ec6772c58
Heidge <axel.gore@outlook.fr>
2023-08-21 16:12:08 +0200

    push task 3 files

--- csharp-classes/3-enemy/3-enemy.cs
+++ csharp-classes/3-enemy/3-enemy.cs
@@ -1,12 +1,43 @@
 ﻿using System;
 
-namespace _3_enemy
+namespace Enemies
 {
-    class Program
+/// <summary>
+/// New class zombie
+/// </summary>
+    public class Zombie
+
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// health field
+        /// </summary>
+        private int health;
+        /// <summary>
+        /// new constructor for zombie
+        /// </summary>
+        public Zombie()
         {
-            Console.WriteLine("Hello World!");
+            health = 0;
+        }
+        /// <summary>
+        /// new constructor with one int parameter mandatory
+        /// </summary>
+        /// <param name="value">value gift to method</param>
+        /// <exception cref="ArgumentException">if value is set to 0 or less, throw this exception</exception>
+        public Zombie(int value)
+        {
+            if (value < 0)
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            else
+                health = value;
+        }
+        /// <summary>
+        /// new method for return heatl value
+        /// </summary>
+        /// <returns>return the value of health field</returns>
+        public int GetHealth()
+        {
+            return health;
         }
     }
 }
