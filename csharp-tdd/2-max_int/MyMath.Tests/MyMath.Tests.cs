685e193a22dd4f9bb3d647919e1388642624c568
Heidge <axel.gore35@gmail.com>
2023-08-23 14:41:17 +0200

    push task 2 files

---
+++ csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
@@ -0,0 +1,24 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace MyMath.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            
+            List<int> list = new List<int>() { 1, 2, 3 };
+            Assert.AreEqual(Operations.Max(list), 3);
+        }
+        [Test]
+        public void Test2()
+        {
+            
+            List<int> list = new List<int>();
+            Assert.AreEqual(Operations.Max(list), 0);
+        }
+    }
+}
---
+++ csharp-tdd/2-max_int/MyMath/MyMath.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyMath
+{
+    /// <summary>
+    /// New class
+    /// </summary>
+    public class Operations
+    {
+       /// <summary>
+       /// New method
+       /// </summary>
+       /// <param name="nums"></param>
+       /// <returns></returns>
+        public static int Max(List<int> nums)
+        {
+            if (nums.Count == 0)
+                return 0;
+            
+            int maxInt = 0;
+
+            for (int i = 0; i < nums.Count; i++) 
+            {
+                if (nums[i] > maxInt)
+                {
+                    maxInt = nums[i];
+                    Console.WriteLine(maxInt);
+                }
+            }
+            return maxInt;
+        }
+    }
+}
