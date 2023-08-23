56cfe50ac7cd124c350168832d5a11219427e117
Heidge <axel.gore@outlook.fr>
2023-08-23 10:53:07 +0200

    push task 0 files

---
+++ csharp-tdd/0-add/MyMath/MyMath.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyMath
+{
+    /// <summary>
+    /// New class
+    /// </summary>
+    public class Operations
+    {
+        /// <summary>
+        /// New method
+        /// </summary>
+        /// <param name="a">first integer</param>
+        /// <param name="b">second integer</param>
+        /// <returns>sum of a and b</returns>
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
