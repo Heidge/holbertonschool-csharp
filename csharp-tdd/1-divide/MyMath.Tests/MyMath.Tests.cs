38f85d3653213935768093961d8d5b7a96843fec
Heidge <axel.gore@outlook.fr>
2023-08-23 12:14:27 +0200

    push task 1 files

---
+++ csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace MyMath.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            int[,] a;
+            int[,] b;
+            a = new int[,] { { 2, 2 }, { 4, 4 }, { 6, 6 }, { 8, 8 } };
+            b = new int[,] { { 1, 1 }, { 2, 2 }, { 3, 3 }, { 4, 4 } };
+            Assert.AreEqual(Matrix.Divide(a, 2), b);
+        }
+    }
+}
---
+++ csharp-tdd/1-divide/MyMath/MyMath.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace MyMath
+{
+    /// <summary>
+    /// New class
+    /// </summary>
+    public class Matrix
+    {
+        /// <summary>
+        /// new method
+        /// </summary>
+        /// <param name="matrix"></param>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public static int[,] Divide(int[,] matrix, int num)
+        {
+            if (matrix == null)
+                return null;
+
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+
+            int[,] new_matrix = new int[rows, cols];
+            try
+            {
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < cols; j++)
+                    {
+                        new_matrix[i, j] = matrix[i, j] / num;
+                    }
+                }
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Num cannot be 0");
+                return null;
+            }
+            return new_matrix;
+
+        }
+    }
+}
