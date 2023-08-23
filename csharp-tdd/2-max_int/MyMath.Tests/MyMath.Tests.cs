5b645606fe3e6f418c74f269ea0848f025d3a546
Heidge <axel.gore35@gmail.com>
2023-08-23 15:08:07 +0200

    push task 2 files

--- csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
+++ csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
@@ -9,14 +9,12 @@ namespace MyMath.Tests
         [Test]
         public void Test1()
         {
-            
-            List<int> list = new List<int>() { -85, -2, -3 };
-            Assert.AreEqual(Operations.Max(list), -2);
+            List<int> list = new List<int>() { -3, -2, -1 };
+            Assert.AreEqual(Operations.Max(list), -1);
         }
         [Test]
         public void Test2()
         {
-            
             List<int> list = new List<int>();
             Assert.AreEqual(Operations.Max(list), 0);
         }
