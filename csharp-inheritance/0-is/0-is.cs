703beae5f35edea04a48f3c817fb8c1451fbecd6
Heidge <axel.gore35@gmail.com>
2023-08-30 15:12:52 +0200

    push multiples directories, task 0 and readme

---
+++ csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _0_is
+{
+    class Obj
+    {
+        public static bool IsOfTypeInt(object obj)
+        {
+            if (obj.GetType() == typeof(int))
+                return true;
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
