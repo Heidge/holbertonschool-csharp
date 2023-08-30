31100572193b6b730b6452b0f5b3d4c6b8ed2adf
Heidge <axel.gore35@gmail.com>
2023-08-30 15:17:40 +0200

    push multiples directories, task 0 and readme

--- csharp-inheritance/0-is/0-is.cs
+++ csharp-inheritance/0-is/0-is.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 
+/// <summary>
+/// New class obj
+/// </summary>
     class Obj
     {
         /// <summary>
-        /// New class named Obj
+        /// New method for check if argument is int or not
         /// </summary>
         /// <param name="obj">first argument passed</param>
         /// <returns>true if obj is int, otherwise false</returns>
         public static bool IsOfTypeInt(object obj)
+        
         {
             if (obj.GetType() == typeof(int))
                 return true;
