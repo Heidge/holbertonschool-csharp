b139c965737aafb9f297c2d5dae7584cda48e203
Heidge <axel.gore35@gmail.com>
2023-09-11 16:13:36 +0200

    push task 9 files

---
+++ csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+    class VectorMath
+    {
+        public static double[] Multiply(double[] vector, double scalar)
+        {
+
+            int vectorLength = vector1.Length;
+            double[] result = new double[vector1Length];
+            double[] error = new double[1] {-1};
+            if (vectorLength < 2 || vectorLength > 3 )
+            {
+                return error;
+            }
+            else
+            {
+                for (int i = 0; i < vectorLength; i++)
+                {
+                    result[i] = vector[i] * scalar;
+                }
+                return result;
+            }
+        }
+    }
