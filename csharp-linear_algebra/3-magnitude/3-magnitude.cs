e8e0f80893ad0048cb1630c84b803ff5dc963209
Heidge <axel.gore35@gmail.com>
2023-09-11 15:20:41 +0200

    push task 2 files

---
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+    class VectorMath
+    {
+        public static double Magnitude(double[] vector)
+        {
+            int vectorLength = vector.Length;
+            int result = 0;
+            if (vectorLength < 2)
+            {
+                return -1;
+            }
+            else
+            {
+                for (int i = 0; i < vectorLength; i++)
+                {
+                    result += Math.Pow(vector[i], 2);
+                    
+                }
+                result = Math.Sqrt(result);
+                return result;
+            }
+        }
+    }
