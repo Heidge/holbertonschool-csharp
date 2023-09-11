f3d2e49128ddbcf0570fe7a46ee7457ade092c31
Heidge <axel.gore35@gmail.com>
2023-09-11 15:56:25 +0200

    push task 6 files

---
+++ csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+    class VectorMath
+    {
+        public static double[] Add(double[] vector1, double[] vector2)
+        {
+
+            int vector1Length = vector1.Length;
+            int vector2Length = vector2.Length;
+            double[] result = new double[];
+            if (vector1Length < 2 || vector1Length > 3 || vector2Length < 2 || vector2Length > 3 || vector1Length != vector2Length)
+            {
+                return -1;
+            }
+            else
+            {
+                for (int i = 0; i < vector1Length; i++)
+                {
+                    result[i] = vector1[i] + vector2[i];
+                }
+                return result;
+            }
+        }
+    }
