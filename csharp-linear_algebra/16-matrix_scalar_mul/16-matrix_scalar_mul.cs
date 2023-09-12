d399d5b74f67c4d39ad1c24ec17cc6ed16794a85
Heidge <axel.gore@outlook.fr>
2023-09-12 11:18:03 +0200

    push task 16 files

---
+++ csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class MatrixMath
+{
+    public static double[,] MultiplyScalar(double[,] matrix, double scalar)
+    {
+        double[,] result = new double[matrix1.GetLength(0), matrix2.GetLength(1)];
+        double[,] error = new double[1, 1] {{ -1 }};
+
+        if (matrix.GetLength(0) != matrix.GetLength(1))
+        {
+            return error;
+        }
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                result[i, j] = matrix[i, j] * scalar;
+            }
+        }
+        return result;
+    }
+}
