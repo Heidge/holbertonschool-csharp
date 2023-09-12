c1dc66ad15da740b943e263e7e0d54fa3a2bf72d
Heidge <axel.gore@outlook.fr>
2023-09-12 10:44:42 +0200

    push task 14 files

--- csharp-linear_algebra/14-matrix_addition/14-main.cs
+++ csharp-linear_algebra/14-matrix_addition/14-main.cs
@@ -4,18 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        double[,] matrix1 = { { -6, 1 }, { -8, 7 } };
-        double[,] matrix2 = { { 9, -2 }, { -1, 2 } };
+        double[,] matrix1 = { { 2, -9, 7 } };
+        double[,] matrix2 = { { 3, -1, 0 } };
         double[,] result;
 
         result = MatrixMath.Add(matrix1, matrix2);
 
-        for (int i = 0; i < matrix1.GetLength(0); i++)
+        for (int i = 0; i < result.GetLength(0); i++)
         {
-            for (int j = 0; j < matrix1.GetLength(1); j++)
+            for (int j = 0; j < result.GetLength(1); j++)
             {
                 Console.Write(result[i, j]);
-                if (j != matrix1.GetLength(1) - 1)
+                if (j != result.GetLength(1) - 1)
                     Console.Write(", ");
             }
             Console.WriteLine();
