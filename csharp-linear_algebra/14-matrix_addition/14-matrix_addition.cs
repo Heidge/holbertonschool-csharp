1ea293d77abe903e679d0a0e5dc24da77ecdcb04
Heidge <axel.gore@outlook.fr>
2023-09-12 10:22:44 +0200

    push task 14 files

--- csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -1,17 +1,23 @@
 ﻿using System;
 
-    class MatrixMath
+class MatrixMath
+{
+    public static double[,] Add(double[,] matrix1, double[,] matrix2)
     {
-        public static double[,] Add(double[,] matrix1, double[,] matrix2)
+        double[,] result = new double[matrix1.GetLength(0), matrix2.GetLength(1)];
+        double[,] error = new double[1, 1] {{ -1 }};
+
+        if (matrix1.GetLength(0) != matrix2.GetLength(0) || matrix1.GetLength(1) != matrix2.GetLength(1))
         {
-            double[,] result = new double[matrix1.GetLength(0), matrix2.GetLength(1)];
-            for (int i = 0; i < matrix1.GetLength(0); i++)
-            {
-                for (int j = 0; j < matrix1.GetLength(1); j++)
-                {
-                    result[i, j] = matrix1[i, j] + matrix2[i, j];
-                }
-            }
-            return result;
+            return error;
         }
+        for (int i = 0; i < matrix1.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix1.GetLength(1); j++)
+            {
+                result[i, j] = matrix1[i, j] + matrix2[i, j];
+            }
+        }
+        return result;
     }
+}
