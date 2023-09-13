ee62c43d7c564a04b84821e3c29b4394ff3bed19
Heidge <axel.gore35@gmail.com>
2023-09-13 16:40:37 +0200

    push task 25 files

---
+++ csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class MatrixMath
+{
+    public static double[,] Transpose(double[,] matrix)
+    {
+        double[,] result = new double[matrix.GetLength(1), matrix.GetLength(0)];
+        double[,] error = new double[1, 1] { { -1 } };
+
+        if (matrix.GetLength(0) != matrix.GetLength(1) || matrix.GetLength(1) < 2)
+        {
+            return error;
+        }
+        for (int i = 0; i < result.GetLength(0); i++)
+        {
+            for (int j = 0; j < result.GetLength(1); j++)
+            {
+                result[i, j] += matrix[j, i];
+            }
+        }
+        return result;
+    }
+}
