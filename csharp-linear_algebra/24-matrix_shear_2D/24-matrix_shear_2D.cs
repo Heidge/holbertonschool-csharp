fbfcb429533a318261182298f3c00f70d40a0d93
Heidge <axel.gore35@gmail.com>
2023-09-13 16:27:45 +0200

    push task 24 files

---
+++ csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class MatrixMath
+{
+    public static double[,] Shear2D(double[,] matrix, char direction, double factor)
+    {
+        double[,] result = new double[matrix.GetLength(0), matrix.GetLength(1)];
+        double[,] error = new double[1, 1] { { -1 } };
+
+        if (matrix1.GetLength(1) != matrix2.GetLength(0))
+        {
+            return error;
+        }
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                if (direction == "X")
+                {
+                    result[i, 0] += Math.Pow(j, factor);
+                }
+                else if (direction == "Y")
+                {
+                    result[0, j] += Math.Pow(i, factor);
+                }
+            }
+        }
+        return result;
+    }
+}
