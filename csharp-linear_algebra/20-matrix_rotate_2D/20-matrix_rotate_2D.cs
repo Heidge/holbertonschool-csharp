240ab7b1bf48f02778d298b833e8b01cbd640044
Heidge <axel.gore35@gmail.com>
2023-09-13 16:11:11 +0200

    push task 20 files

---
+++ csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Windows.Media;
+
+class MatrixMath
+{
+    public static double[,] Rotate2D(double[,] matrix, double angle)
+    {
+        double[,] result = new double[matrix.GetLength(0), matrix.GetLength(1)];
+        matrix.Rotate(angle);
+        return matrix;
+    }
+}
