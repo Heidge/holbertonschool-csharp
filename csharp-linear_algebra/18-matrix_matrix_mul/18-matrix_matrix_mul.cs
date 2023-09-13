0ff16e6f8cd0923d7562a7ac6c8404338df1ba0b
Heidge <axel.gore35@gmail.com>
2023-09-13 15:52:46 +0200

    push task 18 files

--- csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
+++ csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -5,17 +5,21 @@ class MatrixMath
     public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
     {
         double[,] result = new double[matrix1.GetLength(0), matrix2.GetLength(1)];
-        double[,] error = new double[1, 1] {{ -1 }};
+        double[,] error = new double[1, 1] { { -1 } };
 
-        /** if (matrix1.GetLength(0) != matrix2.GetLength(0) || matrix1.GetLength(1) != matrix2.GetLength(1) || matrix1.GetLength(0) != matrix1.GetLength(1))
+        if (matrix1.GetLength(0) != matrix2.GetLength(0) || matrix1.GetLength(1) != matrix2.GetLength(1) || matrix1.GetLength(0) != matrix1.GetLength(1))
         {
             return error;
-        } */
+        }
         for (int i = 0; i < matrix1.GetLength(0); i++)
         {
             for (int j = 0; j < matrix2.GetLength(1); j++)
             {
-                result[i, j] = (matrix1[i, j + 1] * matrix2[i + 1, j]) + (matrix1[i, j] * matrix2[i + j]);
+                for (int k = 0; k < matrix1.GetLength(1); k++)
+                {
+                    result[i, j] += matrix1[i, k] * matrix2[k, j];
+                }
+
             }
         }
         return result;
