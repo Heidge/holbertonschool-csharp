5e4fe8f7f9449a642f1b30d317e994ff89b36952
Heidge <axel.gore35@gmail.com>
2023-09-13 17:07:53 +0200

    push task 26 files

---
+++ csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class MatrixMath
+{
+    public static double Determinant(double[,] matrix)
+    {
+        double result = 0;
+
+        if (matrix.GetLength(0) < 2 || matrix.GetLength(0) > 3)
+        {
+            return -1;
+        }
+
+        if (matrix.GetLength(0) == 2)
+        {
+            result += (matrix[0, 0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0]);
+        }
+        else
+        {
+            result += (matrix[0, 0] * matrix[1, 1] * matrix[2, 2]) + (matrix[0, 1] * matrix[1, 2] * matrix[2, 0]) + (matrix[0, 2] * matrix[1, 0] * matrix[2, 1]) - (matrix[0, 2] * matrix[1, 1] * matrix[2, 0]) - (matrix[0, 1] * matrix[1, 0] * matrix[2, 2]) - (matrix[0, 0] * matrix[1, 2] * matrix[2, 1]);
+        }
+
+        return result;
+    }
+}
