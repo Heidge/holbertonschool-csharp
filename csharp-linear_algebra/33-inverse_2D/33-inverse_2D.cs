73312ae9e1690bbb56078bf9b92176303584f978
Heidge <axel.gore35@gmail.com>
2023-09-14 15:07:32 +0200

    push task 31 files

--- csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
+++ csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
@@ -7,17 +7,17 @@ class MatrixMath
         double determinant = matrix[0, 0] * matrix[1, 1] - matrix[0, 1] * matrix[1, 0];
         double[,] error = new double[1,1] {{-1}};
 
-        if (determinant == 0 || matrix.Length != 2)
+        if (determinant == 0 || matrix.GetLength(0) != 2)
         {
             return error;
         }
 
         double[,] matrixInverse = new double[2, 2];
 
-        matrixInverse[0, 0] = matrix[1, 1] / determinant;
-        matrixInverse[0, 1] = -matrix[0, 1] / determinant;
-        matrixInverse[1, 0] = -matrix[1, 0] / determinant;
-        matrixInverse[1, 1] = matrix[0, 0] / determinant;
+        matrixInverse[0, 0] = Math.Round(matrix[1, 1] / determinant, 2);
+        matrixInverse[0, 1] = Math.Round(-matrix[0, 1] / determinant, 2);
+        matrixInverse[1, 0] = Math.Round(-matrix[1, 0] / determinant, 2);
+        matrixInverse[1, 1] = Math.Round(matrix[0, 0] / determinant, 2);
 
         return matrixInverse;
     }
