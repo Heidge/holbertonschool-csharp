ad14fb536de690e3372437cdab0fccf6efecfc75
Heidge <axel.gore35@gmail.com>
2023-09-14 13:55:25 +0200

    push task 27 files

--- csharp-linear_algebra/28-determinant/28-determinant.cs
+++ csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -19,7 +19,6 @@ class MatrixMath
         {
             result += (matrix[0, 0] * matrix[1, 1] * matrix[2, 2]) + (matrix[0, 1] * matrix[1, 2] * matrix[2, 0]) + (matrix[0, 2] * matrix[1, 0] * matrix[2, 1]) - (matrix[0, 2] * matrix[1, 1] * matrix[2, 0]) - (matrix[0, 1] * matrix[1, 0] * matrix[2, 2]) - (matrix[0, 0] * matrix[1, 2] * matrix[2, 1]);
         }
-
         return Math.Round(result, 2);
     }
 }
