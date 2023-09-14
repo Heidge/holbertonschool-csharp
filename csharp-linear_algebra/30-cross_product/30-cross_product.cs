19290d50c49ff9ab3ce1442dde286c3de52db703
Heidge <axel.gore35@gmail.com>
2023-09-14 14:40:40 +0200

    push task 28 files

--- csharp-linear_algebra/30-cross_product/30-cross_product.cs
+++ csharp-linear_algebra/30-cross_product/30-cross_product.cs
@@ -4,12 +4,14 @@ class VectorMath
 {
     public static double[] CrossProduct(double[] vector1, double[] vector2)
     {
-        if (vector1.Length != vector2.Length || vector1.Length < 3 || vector2.Length < 3)
-        {
-            return -1;
-        }
 
         double[] result = new double[vector1.Length];
+        double[] error = new double[1] {-1};
+
+        if (vector1.Length != vector2.Length || vector1.Length < 3 || vector2.Length < 3)
+        {
+            return error;
+        }
 
         result[0] = vector1[1] * vector2[2] - vector1[2] * vector2[1];
         result[1] = vector1[2] * vector2[0] - vector1[0] * vector2[2];
