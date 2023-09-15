5c4ac5e06850088d1e08b3ba0cd0115b0b88bedb
Heidge <axel.gore35@gmail.com>
2023-09-15 14:41:04 +0200

    fix condition for return -1 when matrix don't have Y

--- csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
+++ csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
@@ -4,16 +4,20 @@ class MatrixMath
 {
     public static double[,] Inverse2D(double[,] matrix)
     {
-        double determinant = matrix[0, 0] * matrix[1, 1] - matrix[0, 1] * matrix[1, 0];
-        double[,] error = new double[1,1] {{-1}};
+        double[,] error = new double[1, 1] { { -1 } };
+        double[,] matrixInverse = new double[2, 2];
 
-        if (determinant == 0 || matrix.GetLength(0) != 2)
+        if (matrix.GetLength(1) < 2)
         {
             return error;
         }
 
-        double[,] matrixInverse = new double[2, 2];
+        double determinant = matrix[0, 0] * matrix[1, 1] - matrix[0, 1] * matrix[1, 0];
 
+        if (matrix.GetLength(0) < 2)
+        {
+            return error;
+        }
         matrixInverse[0, 0] = Math.Round(matrix[1, 1] / determinant, 2);
         matrixInverse[0, 1] = Math.Round(-matrix[0, 1] / determinant, 2);
         matrixInverse[1, 0] = Math.Round(-matrix[1, 0] / determinant, 2);
