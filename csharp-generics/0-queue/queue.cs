318cf569cb6d49e14af82a23fbdaa0ff3656c66f
Heidge <axel.gore@outlook.fr>
2023-11-07 11:23:37 +0100

    push task 0

---
+++ csharp-generics/0-queue/queue.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+    public class Queue<T>
+    {
+        public Type CheckType()
+        {
+            Type typeParameterType = typeof(T);
+            return(typeParameterType);
+        }
+    }
