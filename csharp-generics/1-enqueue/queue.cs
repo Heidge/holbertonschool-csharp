d3cb6308d8d3504f23cf6c37c0792ee46cb34a5d
Heidge <axel.gore35@gmail.com>
2023-11-09 16:18:19 +0100

    push task 1

---
+++ csharp-generics/1-enqueue/queue.cs
@@ -0,0 +1,53 @@
+﻿using System;
+/// <summary>
+/// New generic list Queue of type T
+/// </summary>
+/// <typeparam name="T"></typeparam>
+class Queue<T>
+{
+    /// <summary>
+    /// New function for check type
+    /// </summary>
+    /// <returns>Type of T</returns>
+    /// 
+    public Node head;
+    public Node tail;
+    int count = 0;
+
+    public class Node
+    {
+        /// <summary>
+        /// New class for object Node
+        /// </summary>
+        public T value = default(T);
+        public Node next = null;
+
+        public Node(T value)
+        {
+            this.value = value;
+        }
+    }
+
+    public void Enqueue(T p)
+    {
+        Node n = new Node(p); 
+        if (head == null)
+        {
+            head = n;
+        }
+        else
+        {
+            tail = n;
+        }
+        count++;
+    }
+
+    public int Count()
+    { return count; }
+
+
+
+
+
+}
+
