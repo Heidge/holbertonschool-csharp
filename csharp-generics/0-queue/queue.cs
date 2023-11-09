9620afdbd75ca0e344702a0e9cf804d7588785d2
Heidge <axel.gore@outlook.fr>
2023-11-09 15:39:23 +0100

    Add csharp generics directory

--- csharp-generics/0-queue/queue.cs
+++ csharp-generics/0-queue/queue.cs
@@ -3,14 +3,30 @@
 /// New generic list Queue of type T
 /// </summary>
 /// <typeparam name="T"></typeparam>
-    class Queue<T>
+class Queue<T>
+{
+    /// <summary>
+    /// New function for check type
+    /// </summary>
+    /// <returns>Type of T</returns>
+    Node head = new Node;
+    Node tail = new Node;
+    int count;
+    public class Node
     {
-        /// <summary>
-        /// New function for check type
-        /// </summary>
-        /// <returns>Type of T</returns>
-        public Type CheckType()
+        Node next = null;
+        int value = null;
+        public Node(Node value)
         {
-            return(typeof(T));
+            Value = value;
         }
+
+        public string Value
+        {
+            set;
+        }
+
+
     }
+}
+
--- csharp-generics/1-enqueue/1-enqueue.cs
+++ csharp-generics/1-enqueue/1-enqueue.cs
@@ -1,9 +1,36 @@
 ﻿using System;
-
-    class Program
+/// <summary>
+/// New generic list Queue of type T
+/// </summary>
+/// <typeparam name="T"></typeparam>
+class Queue<T>
+{
+    /// <summary>
+    /// New function for check type
+    /// </summary>
+    /// <returns>Type of T</returns>
+    Node head;
+    Node tail;
+    int count;
+    public class Node
     {
-        static void Main(string[] args)
+        Node next = null;
+        T value = null;
+        public Node(Node value)
         {
-            Console.WriteLine("Hello World!");
+            Value = value;
         }
     }
+
+    public void Enqueue(T t)
+    {
+        Node n = new Node(t);
+        n.Value = head;
+        head = n;
+    }
+
+    public int Count()
+    {
+        return this.Count;
+    }
+}
