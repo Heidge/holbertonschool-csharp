040b4a4c83ef50022de44fce1d2994ecaa11e875
Heidge <axel.gore35@gmail.com>
2023-11-12 17:40:08 +0100

    push task 3

--- csharp-interfaces/3-decorations/3-decorations.cs
+++ csharp-interfaces/3-decorations/3-decorations.cs
@@ -1,12 +1,98 @@
 ﻿using System;
+using System.Collections;
 
-namespace _3_decorations
+/// <summary>Interface 1 - interactive</summary>
+interface IInteractive
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	void Interact();
+}
+
+/// <summary>Interface 2 - breakable</summary>
+interface IBreakable
+{
+	int durability { get; set; }
+	void Break();
+}
+
+/// <summary>Interface 3 - collectable</summary>
+interface ICollectable
+{
+	bool isCollected { get; set; }
+	void Collect();
+}
+
+/// <summary>abstract class base</summary>
+public abstract class Base
+{
+	/// <summary>public property name</summary>
+	public string name { get; set; }
+
+	/// <summary>Method override ToString</summary>
+	/// <returns>The value of name and his type</returns>
+	public override string ToString()
+	{
+		return name + $" is a " + this.GetType().Name;
+	}
+}
+
+/// <summary>inherit class from base with 3 interfaces implementation</summary>
+public class Door : Base, IInteractive
+{
+	/// <summary>
+	/// Constructor for door
+	/// </summary>
+	/// <param name="name">name</param>
+	public Door(string name = "Door")
+	{
+		this.name = name;
+	}
+
+	/// <summary>
+	/// interact metho from interactive interface
+	/// </summary>
+	public void Interact()
+	{
+		Console.WriteLine($"You try to open the {name}. It's locked.");
+	}
+
+}
+
+public class Decoration : Base, IInteractive, IBreakable
+{
+	public bool isQuestItem { get; set; }
+	public int durability { get; set; }
+
+	public Decoration (string name = "Decoration", int durability = 1, bool IsQuestItem = false)
+	{
+		this.name = name;
+		this.durability = durability;
+		this.isQuestItem = isQuestItem;
+
+		if (durability <= 0)
+			throw new Exception("Durability must be greather than 0");
+	}
+
+	public void Interact()
+	{
+		if (durability <= 0)
+			Console.WriteLine($"The {name} has been broken");
+		else
+			if (isQuestItem) 
+				Console.WriteLine($"You look at the {name}. There's a key inside.");
+			else
+				Console.WriteLine($"You look at the {name}. Not much to see here.");
+	}
+
+	public void Break()
+	{
+
+		durability--;
+		if (durability >= 0)
+			Console.WriteLine($"You hit the {name}. It cracks.");
+		else if (durability == 0)
+			Console.WriteLine($"You hit the {name}. What a mess.");
+		else
+			Console.WriteLine($"The {name} is already broken.");
+
+	}
 }
